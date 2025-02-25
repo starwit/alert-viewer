1bfa8aa6a843a8ce4e266d9b96e066d94eaacda0
Anett Hübner <anett_huebner@gmx.de>
2025-02-25 14:25:07 +0100

    added helm chart

--- application/src/main/java/de/starwit/data/AlertDb.java
+++ application/src/main/java/de/starwit/data/AlertDb.java
@@ -5,19 +5,30 @@ import java.util.List;
 
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.annotation.ApplicationScope;
+import de.starwit.rest.AlertDto;
 
 @ApplicationScope
 @Component
 public class AlertDb {
 
-    private List<String> alerts = new ArrayList<>();
+    private List<AlertDto> alerts = new ArrayList<>();
 
-    public List<String> getAlerts() {
+    private List<AlertDto> marker = new ArrayList<>();
+
+    public List<AlertDto> getAlerts() {
         return alerts;
     }
 
-    public void setAlerts(List<String> alerts) {
+    public void setAlerts(List<AlertDto> alerts) {
         this.alerts = alerts;
     }
-    
+
+    public List<AlertDto> getMarker() {
+        return marker;
+    }
+
+    public void setMarker(List<AlertDto> marker) {
+        this.marker = marker;
+    }
+
 }
--- application/src/main/java/de/starwit/rest/AlertController.java
+++ application/src/main/java/de/starwit/rest/AlertController.java
@@ -3,6 +3,8 @@ package de.starwit.rest;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,6 +24,8 @@ import de.starwit.data.AlertDb;
 @RequestMapping("/api/alerts")
 public class AlertController {
 
+    static final Logger LOG = LoggerFactory.getLogger(AlertController.class);
+
     @Autowired
     private AlertDb alertDb;
 
@@ -34,24 +38,35 @@ public class AlertController {
         }
     }
 
-    @GetMapping("/{name}")
-    public ResponseEntity<?> find(@PathVariable String name) {
+    @GetMapping("/marker")
+    public ResponseEntity<?> findAllMarker() {
         try {
-            alertDb.getAlerts().add(name);
-            return new ResponseEntity<>("GetOne Result: " + name, HttpStatus.OK);
+            return new ResponseEntity<>(alertDb.getMarker(), HttpStatus.OK);
         } catch (Exception e) {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
+    @GetMapping("/{name}/{lat}/{lng}")
+    public ResponseEntity<?> find(@PathVariable String name, @PathVariable String lat, @PathVariable String lng) {
+        try {
+            if (lat != null && lng != null) {
+                AlertDto alert = new AlertDto(name, Double.parseDouble(lat), Double.parseDouble(lng));
+                alertDb.getAlerts().add(alert);
+                alertDb.getMarker().add(alert);
+                return new ResponseEntity<>("GetOne Result: " + alert.getName(), HttpStatus.OK);
+            }
+        } catch (Exception e) {
+            LOG.error("Error parsing geocoordinates.", e);
+        }
+        return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
     @Scheduled(fixedDelayString = "${alert.delay:60s}")
     public void executeActions() {
-        List<String> alerts = new ArrayList<>();
-        List<String> oldAlerts = alertDb.getAlerts();
-        int max = Math.min(oldAlerts.size(), 5);
-        for (int i = 0; i < max; i++) {
-            alerts.add(oldAlerts.get(i));
-        }
+        List<AlertDto> alerts = new ArrayList<>();
+        List<AlertDto> oldAlerts = alertDb.getAlerts();
+        alerts.addAll(oldAlerts);
         alertDb.setAlerts(alerts);
     }
 
@@ -67,8 +82,6 @@ public class AlertController {
     @PutMapping()
     public ResponseEntity<?> update(@RequestBody AlertDto dto) {
         try {
-            // TODO Implement Your Logic To Update Data And Return Result Through
-            // ResponseEntity
             return new ResponseEntity<>("Update Result", HttpStatus.OK);
         } catch (Exception e) {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
@@ -78,8 +91,16 @@ public class AlertController {
     @DeleteMapping("/{name}")
     public ResponseEntity<?> delete(@PathVariable String name) {
         try {
-            List<String> oldAlerts = alertDb.getAlerts();
-            oldAlerts.removeIf(n -> n.equals(name));
+            List<AlertDto> oldAlerts = alertDb.getAlerts();
+            List<AlertDto> toDelete = new ArrayList<>();
+            if (oldAlerts != null && oldAlerts.size() > 0) {
+                for (AlertDto alertDto : oldAlerts) {
+                    if (name != null && name.equals(alertDto.getName())) {
+                        toDelete.add(alertDto);
+                    }
+                }
+                oldAlerts.removeAll(toDelete);
+            }
             return new ResponseEntity<>("Destroy Result", HttpStatus.OK);
         } catch (Exception e) {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
